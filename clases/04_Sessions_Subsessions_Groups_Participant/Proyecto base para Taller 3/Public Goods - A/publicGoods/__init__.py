9080d9f36c5668c41707df1c0428d0f205fecc4a
valentinadiaz2911-afk <valentinadiaz2911@gmail.com>
2025-09-02 16:28:42 -0500

    Update __init__.py

--- clases/04_Sessions_Subsessions_Groups_Participant/Proyecto base para Taller 3/Public Goods - A/publicGoods/__init__.py
+++ clases/04_Sessions_Subsessions_Groups_Participant/Proyecto base para Taller 3/Public Goods - A/publicGoods/__init__.py
@@ -9,18 +9,18 @@ Juego de Public Goods
 class C(BaseConstants):
     NAME_IN_URL = 'publicGoods'
     PLAYERS_PER_GROUP = 3
-
+    NUM_ROUNDS = 1
     ENDOWMENT = cu(10000)
     MULTIPLIER = 2
 
 
 class Subsession(BaseSubsession):
-
-
-
+    pass
+    
+    
 class Group(BaseGroup):
-    total_contribution = 
-
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
 
 class Player(BasePlayer):
     contribution = models.CurrencyField(
@@ -37,7 +37,8 @@ def set_payoffs(group):
     contributions = [p.contribution for p in players]
     group.total_contribution = sum(contributions)
     group.individual_share = (group.total_contribution * C.MULTIPLIER) / C.PLAYERS_PER_GROUP
-
+    for p in players:
+        p.payoff = C.ENDOWMENT - p.contribution + group.individual_share
 
 # PAGES
 class MyPage(Page):
